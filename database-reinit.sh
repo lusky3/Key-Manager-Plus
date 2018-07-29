03a17187b179335b3a7e31feb48a654db867be98
ChaosZero112 <ChaosZero112@users.noreply.github.com>
2018-07-29 19:12:49 -0400

    Copy reinitializeDB.sh to the correct location

--- database-reinit.sh
+++ database-reinit.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # Reinitialize the Keymanager database if no data found
-if [ "$(ls -A /opt/ManageEngine/Keymanager/pgsql/data)" ]; then
+if [ "$(ls -A /opt/ManageEngine/Keymanager/pgsql/data/base)" ]; then
     echo "Database exists."
     exit 0
 else
+    cp /opt/ManageEngine/Keymanager/bin/reinitializeDB.sh /opt/ManageEngine/Keymanager/scripts/reinitializeDB.sh
     sh /opt/ManageEngine/Keymanager/bin/keymanager.sh reinit
 fi
